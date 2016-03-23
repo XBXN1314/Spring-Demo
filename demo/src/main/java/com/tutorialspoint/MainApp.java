b7ab0d37c3451e67b521dcc94c1188d447217c99
XBXN1314 <530996947@qq.com>
2016-03-23 20:01:21 +0800

    bean 生命周期

--- demo/src/main/java/com/tutorialspoint/MainApp.java
+++ demo/src/main/java/com/tutorialspoint/MainApp.java
@@ -1,6 +1,7 @@
 package com.tutorialspoint;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -8,11 +9,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class MainApp {
     public static void main(String [] args){
-        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
         HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-        obj.setMessage("I'm a object!");
+//        obj.setMessage("I'm a object!");
         obj.getMessage();
-        HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
-        objA.getMessage();
+//        HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+//        objA.getMessage();
+        context.registerShutdownHook();
     }
 }
